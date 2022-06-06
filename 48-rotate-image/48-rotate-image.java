55ecd39f608ee1257fb8cceb0f00dc0e88601ade
Devansh Kashyap <47043096+dkashyap23@users.noreply.github.com>
2022-06-06 18:42:43 +0530

    Time: 0 ms (100.00%), Space: 43 MB (25.73%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void rotate(int[][] mat) {
+        int n=mat.length;
+        //First make transope of the matrix
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                int t=mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i]=t;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            int s=0;
+            int e=n-1;
+            while(s<e)
+            {
+                int x=mat[i][s];
+                mat[i][s]=mat[i][e];
+                mat[i][e]=x;
+                s++;
+                e--;
+            }
+        }
+        //return mat;
+    }
+}
