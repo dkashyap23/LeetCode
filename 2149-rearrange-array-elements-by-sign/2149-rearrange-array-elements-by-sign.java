b2ce45d079f8fe6d4f2e517b46e1dcd0f5c369ff
Devansh Kashyap <47043096+dkashyap23@users.noreply.github.com>
2022-06-16 11:12:23 +0530

    Time: 7 ms (64.90%), Space: 223.2 MB (45.14%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,27 +1,48 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int s=0;
-        int e=0;
+        //1st approach
+        // int s=0;
+        // int e=0;
+        // int n=nums.length;
+        // int[] pos=new int[n/2];
+        // int[] neg=new int[n/2];
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]>0)
+        //         pos[s++]=nums[i];
+        //     else
+        //         neg[e++]=nums[i];
+        // }
+        // s=0;
+        // e=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(i%2==0)
+        //         nums[i]=pos[s++];
+        //     else
+        //         nums[i]=neg[e++];
+        // }
+        // return nums;
+        
+        //2nd approach
         int n=nums.length;
-        int[] pos=new int[n/2];
-        int[] neg=new int[n/2];
+        int[] ans=new int[n];
+        int ind=0;
+        int indneg=1;
         for(int i=0;i<n;i++)
         {
             if(nums[i]>0)
-                pos[s++]=nums[i];
+            {
+                ans[ind]=nums[i];
+                ind+=2;
+            }
             else
-                neg[e++]=nums[i];
+            {
+                ans[indneg]=nums[i];
+                indneg+=2;
+            }
         }
-        s=0;
-        e=0;
-        for(int i=0;i<n;i++)
-        {
-            if(i%2==0)
-                nums[i]=pos[s++];
-            else
-                nums[i]=neg[e++];
-        }
-        return nums;
+        return ans;
             
     }
 }
