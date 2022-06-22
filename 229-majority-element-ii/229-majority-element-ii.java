098aa2943b980eb53e1dfd9bc76617afe4fe58c0
Devansh Kashyap <47043096+dkashyap23@users.noreply.github.com>
2022-06-22 23:49:54 +0530

    Time: 3 ms (79.41%), Space: 48.9 MB (78.92%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,16 +1,51 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> mp=new HashMap<>();
-        int n=nums.length;
         List<Integer> ans=new ArrayList<>();
-        for(int i=0;i<nums.length;i++)
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-        for(Map.Entry<Integer,Integer> m:mp.entrySet())
+        int n=nums.length;
+        int candidate1=Integer.MIN_VALUE;
+        int candidate2=Integer.MIN_VALUE;
+        int life1=0;
+        int life2=0;
+        for(int i=0;i<n;i++)
         {
-            if(m.getValue()>n/3)
-                ans.add(m.getKey());
+            if(candidate1==nums[i])
+                life1++;
+            else if(candidate2==nums[i])
+                life2++;
+            else if(life1==0)
+            {
+                candidate1=nums[i];
+                life1=1;
+            }
+            else if(life2==0)
+            {
+                candidate2=nums[i];
+                life2=1;
+            }
+            else
+            {
+                life1--;
+                life2--;
+            }
         }
+        life1=0;
+        life2=0;
+        for(int i=0;i<n;i++)
+        {
+            if(candidate1==nums[i])
+                life1++;
+            if(candidate2==nums[i])
+                life2++;
+        }
+        if(life1>(n/3) && life2>(n/3))
+        {
+            ans.add(candidate1);
+             ans.add(candidate2);
+        }
+        else if(life1>n/3)
+             ans.add(candidate1);
+        else if(life2>n/3)
+             ans.add(candidate2);
         return ans;
-        
     }
 }
