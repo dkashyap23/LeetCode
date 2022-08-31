fc706b1b0a132af4521043d0ae758bdac1a90afd
Devansh Kashyap <47043096+dkashyap23@users.noreply.github.com>
2022-09-01 01:35:33 +0530

    Time: 2 ms (97.69%), Space: 42.3 MB (99.34%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -1,30 +1,13 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int max=nums[0];
-        int max_ind=0;
-        int n=nums.length;
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(nums[i]>max)
-        //     {
-        //         max=nums[i];
-        //         max_ind=i;
-        //     }
-        // }
-        // System.out.println(max+" "+max_ind);
         int ans=0;
-        // if(max_ind==-1)
-        //     return 0;
-        for(int i=1;i<n;i++)
+        for(int i=1;i<nums.length;i++)
         {
-            // ans+=(max-nums[i]+1);
-            // max=max+1;
             if(nums[i-1]>=nums[i])
             {
                 ans+=(nums[i-1]-nums[i]+1);
                 nums[i]=nums[i-1]+1;
             }
-            System.out.print(nums[i]+" ");
         }
         return ans;
     }
