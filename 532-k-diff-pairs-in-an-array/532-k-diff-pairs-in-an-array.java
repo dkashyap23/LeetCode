16d18c1761805950c93078db8f3923a117a9bdd3
Devansh Kashyap <47043096+dkashyap23@users.noreply.github.com>
2022-09-11 20:19:59 +0530

    Time: 31 ms (15.11%), Space: 48.4 MB (36.08%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -1,39 +1,16 @@
 class Solution {
     public int findPairs(int[] A, int B) {
-        int n=A.length;
-        Arrays.sort(A);
-        int count=0;
-        int i=0;
-        int j=1;
-        HashSet<String> set=new HashSet<>();
-        while(j<n)
-        {
-            if(A[j]-A[i]==B)
+          int n=A.length;
+            HashMap<Integer,Integer> mp=new HashMap<>();
+            for(int i=0;i<n;i++)
+                mp.put(A[i],mp.getOrDefault(A[i],0)+1);
+            int count=0;
+            for(int i:mp.keySet())
             {
-                String a=Integer.toString(A[j]);
-                String b=Integer.toString(A[i]);
-                String s=a+"-"+b;
-                //System.out.println(s);
-                if(set.contains(s)==false)
-                {
-                set.add(s);
-                count++;
-                }
-                i++;    
-                j++;
-                
+                if(B>0 && mp.containsKey(i+B) || B==0 & mp.get(i)>1)
+                    count++;
             }
-            else if(A[j]-A[i]>B)
-            {
-                i++;
-                if(i+1>j)
-                    j++;
-            }
-            else
-                j++;
-        }
-        return count;
-
+            return count;
         
     }
 }
