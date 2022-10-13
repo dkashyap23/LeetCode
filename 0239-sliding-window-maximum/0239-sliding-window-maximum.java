fed446c2d014ad6f0f4e763fcc9a8f1daf93cab6
Devansh Kashyap <47043096+dkashyap23@users.noreply.github.com>
2022-10-14 01:19:40 +0530

    Time: 91 ms (26.69%), Space: 150.1 MB (52.82%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,30 +1,40 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        ArrayList<Integer> temp_ans=new ArrayList<>();
+        //ArrayList<Integer> temp_ans=new ArrayList<>();
         Deque<Integer> q=new LinkedList<>();
         int n=nums.length;
+        int[] ans=new int[n-k+1];
         int s=0;
         int e=0;
+        int i=0;
         while(s<=e && e<n)
         {
-            if(q.isEmpty())
-            {
-                q.add(nums[e]);
-                //e++;
-            }
-            else if(q.getLast()<nums[e] )
+            // if(q.isEmpty())
+            // {
+            //     q.add(nums[e]);
+            //     //e++;
+            // }
+            // else if(q.getLast()<nums[e] )
+            // {
+            //     while(!q.isEmpty() && q.getLast()<nums[e])
+            //         q.removeLast();
+            //     q.add(nums[e]);
+            //    // e++;
+            // }
+            // else
+            //     q.add(nums[e]);
+           // System.out.print(q);
+            if(!q.isEmpty() &&q.getLast()<nums[e])
             {
                 while(!q.isEmpty() && q.getLast()<nums[e])
                     q.removeLast();
-                q.add(nums[e]);
-               // e++;
+                
             }
-            else
-                q.add(nums[e]);
-           // System.out.print(q);
+            q.add(nums[e]);
             if(e-s+1==k)
             {
-                temp_ans.add(q.peek());
+                // temp_ans.add(q.peek());
+                ans[i++]=q.peek();
                 if(nums[s]==q.peek())
                     q.remove();
                 s++;
@@ -34,9 +44,9 @@ class Solution {
             
             
         }
-        int[] ans=new int[temp_ans.size()];
-        for(int i=0;i<temp_ans.size();i++)
-                ans[i]=temp_ans.get(i);
+        // int[] ans=new int[temp_ans.size()];
+        // for(int i=0;i<temp_ans.size();i++)
+        //         ans[i]=temp_ans.get(i);
         return ans;
         
     }
