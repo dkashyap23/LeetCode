dae24f08316d251eedc71e0f8a43729e6201bc9f
Devansh Kashyap <47043096+dkashyap23@users.noreply.github.com>
2022-10-18 18:12:37 +0530

    Time: 8 ms (14.07%), Space: 42.6 MB (80.50%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -1,26 +1,25 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int largest=-1;
-        int sec_largest=-1;
-        int ind=0;
+        int largest=0;
+        int sec_largest=0;
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]>largest)
             {
-                largest=nums[i];
-                ind=i;
+                sec_largest=largest;
+                largest=nums[i];  
             }
-                
+            else if(nums[i]>sec_largest)
+                sec_largest=nums[i];
                 
             // if(nums[i]>sec_largest && sec_largest<largest)
             //     sec_largest=nums[i];
         }
-        nums[ind]=-1;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]>sec_largest)
-                sec_largest=nums[i];
-        }
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i]>sec_largest && sec_largest<largest)
+        //         sec_largest=nums[i];
+        // }
         System.out.println(largest+" "+sec_largest);
         return (largest-1)*(sec_largest-1);
         
